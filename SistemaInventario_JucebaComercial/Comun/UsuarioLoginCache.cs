8743a34ff12bea6d2a1dd98118a3e9ac3f263584
Cesar Baez <baezcesar329@gmail.com>
2021-11-04 17:03:08 -0400

    Creacion de la clase UsuarioLoginCache

---
+++ SistemaInventario_JucebaComercial/Comun/UsuarioLoginCache.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comun
+{
+    public static class UsuarioLoginCache
+    {
+        public static int Codigo_usuario { get; set; }
+        public static int Codigo_tipo_usuario { get; set; }
+        public static string Tipo_usuario { get; set; }
+        public static string Nombre_usuario { get; set; }
+        public static string Nombre { get; set; }
+        public static string Email { get; set; }
+        public static string Password { get; set; }
+        public static bool Estado { get; set; }
+    }
+}
